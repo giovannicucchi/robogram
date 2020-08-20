926b482a747325fd0e006b239871889a3cec68e7
Giovanni Cucchi <giovanni@player2.tech>
2020-08-20 16:29:34 -0300

    Updates

---
+++ precache-manifest.6c20bf8d6bb39f93205dc6384a197d32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c75720964d418c3dd42f7d8f17281f",
+    "url": "/giovannicucchi/robogram/index.html"
+  },
+  {
+    "revision": "540f5a253b3e2d0ac388",
+    "url": "/giovannicucchi/robogram/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e8a18642b71bde1b646c",
+    "url": "/giovannicucchi/robogram/static/css/main.5448b348.chunk.css"
+  },
+  {
+    "revision": "540f5a253b3e2d0ac388",
+    "url": "/giovannicucchi/robogram/static/js/2.d478f94c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giovannicucchi/robogram/static/js/2.d478f94c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a18642b71bde1b646c",
+    "url": "/giovannicucchi/robogram/static/js/main.33d6184a.chunk.js"
+  },
+  {
+    "revision": "e7325ea991a4b6e80d35",
+    "url": "/giovannicucchi/robogram/static/js/runtime-main.78c7487f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/giovannicucchi/robogram/static/media/SEGA.12a3db8b.woff"
+  }
+]);
