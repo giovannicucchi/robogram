73db64e250bffbbdea4072ffbe453a204a6ce1a2
Giovanni Cucchi <giovanni@player2.tech>
2020-08-20 16:23:18 -0300

    robogram pronto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import App from './containers/App';
+import './fonts/style.css'
 
 ReactDOM.render(
   <React.StrictMode>
